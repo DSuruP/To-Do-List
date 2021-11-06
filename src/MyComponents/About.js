6afe3414c65e50334cd4a4ba2fc8df4062fc8003
Survesh Pandit <84669149+DSuruP@users.noreply.github.com>
2021-11-06 11:06:24 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an About Component
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Numquam impedit consequuntur a unde mollitia voluptatum soluta eum, 
+                molestiae ipsam ea enim ducimus cupiditate, perferendis corrupti minus esse. 
+                Possimus ratione eius iste nostrum quod animi.
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Quos mollitia sit earum est similique nemo, 
+                molestias quam a vero iste nulla dolores doloremque error, 
+                ipsum animi eveniet eligendi rem amet!
+            </p>
+        </div>
+    )
+}
